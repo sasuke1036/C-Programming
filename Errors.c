fb36f768344ca27088ff1eb39246a67463c64c2f
sasuke1036 <yashwanthkota36@gmail.com>
2021-11-07 15:54:03 +0530

    Resolved the errors in the first chapter

---
+++ Errors.c
@@ -0,0 +1,12 @@
+//Libraries
+#include <stdio.h>
+
+//Main Function
+
+int main()
+{
+ 	int m1,m2,m3;
+ 	printf("Entered values of marks in 3 subjects");
+ 	scanf("%d%d%d",&m1,&m2,&m3);
+ 	printf("You entered %d%d%d",m1,m2,m3);
+}
