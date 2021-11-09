ab000f9fd0840ba00dbda71483a7a329b9dbaecc
sasuke1036 <yashwanthkota36@gmail.com>
2021-11-09 10:58:03 +0530

    Created a file to rectify the errors

--- Errors.c
+++ Errors.c
@@ -5,8 +5,11 @@
 
 int main()
 {
- 	int m1,m2,m3;
- 	printf("Entered values of marks in 3 subjects");
- 	scanf("%d%d%d",&m1,&m2,&m3);
- 	printf("You entered %d%d%d",m1,m2,m3);
+ 	int i;
+ 	float b;
+ 	i=3.8;
+ 	b=30;
+
+ 	printf("i = %d\nb = %f\n",i,b);
+ 	return 0;
 }
