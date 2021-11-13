7888c837396357e4e058c8760488bf681179558b
sasuke1036 <yashwanthkota36@gmail.com>
2021-11-13 20:23:08 +0530

    Created a file t find out the number is odd or even

---
+++ Integer.c
@@ -0,0 +1,18 @@
+// Libraries
+#include <stdio.h>
+
+// Main Function
+
+int main()
+{
+	int x,even,odd;
+
+	printf("Enter the value of x : ");
+	scanf("%d",&x);
+
+	if(x%2 == 1)
+		printf("odd\n");
+	else
+		printf("even\n");
+	return 0;
+}
