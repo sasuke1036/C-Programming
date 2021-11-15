6cbeab00a287c75bf29263623d29fa146ab9367b
sasuke1036 <yashwanthkota36@gmail.com>
2021-11-15 23:00:35 +0530

    Created a file to know the location on the coordinate axes

---
+++ Location.c
@@ -0,0 +1,23 @@
+// Libraries
+#include <stdio.h>
+
+// Main Function
+
+int main()
+{
+	int x,y;
+	printf("Enter the value of x : ");
+	scanf("%d",&x);
+
+	printf("Enter the value of y : ");
+	scanf("%d",&y);
+
+	if(y==0,x!=0)
+		printf("point lies on x-axis\n");
+	else if(x==0,y!=0)
+			printf("point lies on y-axis\n");
+	else if (x==0 && y==0)
+		printf("point is origin\n");
+
+	return 0;
+}
