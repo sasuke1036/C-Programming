a30ee9dc733f0e4db53e89d1291d1a1cc877a500
sasuke1036 <yashwanthkota36@gmail.com>
2021-11-15 22:59:53 +0530

    Created a file to know the location around the circle

---
+++ Distance_Parameters.c
@@ -0,0 +1,33 @@
+//Libraries
+#include <math.h>
+#include <stdio.h>
+
+//Main Function
+
+int main()
+{
+	int x,y,radius,distance,a,p;
+
+	printf("Enter the value of x : ");
+	scanf("%d",&x);
+
+	printf("Enter the value of y :");
+	scanf("%d",&y);
+
+	printf("Enter the value of radius : ");
+	scanf("%d",&radius);
+
+	printf("Enter the value of a : ");
+	scanf("%d",&a);
+
+	printf("Enter the value of p : ");
+	scanf("%d",&p);
+
+	distance = sqrt( pow((x-a),2) + pow((y-p),2) );
+
+	if(distance<radius)
+		printf("point lies inside the circle\n");
+	else
+		printf("point lies outside the circle\n");
+	return 0;
+}
