a37bb6b8aeba8a8ba2278e065216a09ba3678335
sasuke1036 <yashwanthkota36@gmail.com>
2021-11-20 22:32:21 +0530

    Modified few files

--- ASCII.C
+++ ASCII.C
@@ -12,6 +12,21 @@ int main()
 
 	int p = (int)x;
 
-	printf("ASCII value of %c is %d\n",x,p);
+	if(p<=90 && p>=65)
+	{
+		printf("It's a Capital Letter\n");
+	}
+	else if(p>=97 && p<=122)
+	{
+		printf("It's a Small Letter\n");
+	}
+	else if(p>=48 && p<=57)
+	{
+		printf("It's a Single Number\n");
+	}
+	else
+	{
+		printf("It's a Special Symbol\n");
+	}
 	return 0;
 }
--- Leapyear.c
+++ Leapyear.c
@@ -5,14 +5,33 @@
 
 int main()
 {
-	int Calender_year;
+	int Calender_year,days;
 
 	printf("Enter the value of Calender year : ");
 	scanf("%d",&Calender_year);
 
 	if(Calender_year%4 == 0)
-		printf("Leap year\n");
+	{
+		if(Calender_year%100 == 0)
+		{
+			if(Calender_year%400 == 0)
+			{
+				printf("Leap year\n");
+			}
+			else 
+			{
+				printf("Not a leap year\n"); 
+			}	
+		}	
+		else
+		{
+			printf("leap year\n");
+		}
+	}
 	else
+	{
+
 		printf("Not a leap year\n");
+	}
 	return 0;
 }
