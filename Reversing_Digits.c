257c7a9802f210cfad8ff0ce65fbdc49b876949d
sasuke1036 <yashwanthkota36@gmail.com>
2021-11-20 16:01:03 +0530

    Created a file to reverse a number

--- Reversing_Digits.c
+++ Reversing_Digits.c
@@ -5,28 +5,35 @@
 
 int main()
 {
-	int Five_digit_number,a,b,c,d,e,k,Reverse_digit;
+	int Five_digit_number,a,Reverse_digit;
 
-	printf("Ener the value of Five_digit_number : ");
+	printf("Enter the value of Five_digit_number : ");
+
 	scanf("%d",&Five_digit_number);
 
-	k = Five_digit_number ;
-	a = k%10;
-	k = k/10;
-	b = k%10;
-	k = k/10;
-	c = k%10;
-	k = k/10;
-	d = k%10;
-	e = k/10;
+	a = Five_digit_number;
+	Reverse_digit = (a%10)*10000;
+	a /= 10;
+	
+	Reverse_digit += (a%10)*1000;
+	a /= 10;
+	
+	Reverse_digit += (a%10)*100;
+	a /= 10;
+	
+	Reverse_digit += (a%10)*10;
+	a /= 10;
+	
+	Reverse_digit += a;
 
-	Reverse_digit = a*10000+b*1000+c*100+d*10+e;
 	if (Reverse_digit==Five_digit_number)
-
 	{
 		printf("Equal\n");
 	}
 	else
+	{
 		printf("Not Equal\n");
+	}
+
 	return 0;
 }
