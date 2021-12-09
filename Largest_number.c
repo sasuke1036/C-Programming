e344ab4aae6f9d7e460b2f9396e45427e8e349a5
sasuke1036 <yashwanthkota36@gmail.com>
2021-12-09 22:49:16 +0530

    Created a file to know first and second largest numbers in a given list

--- Largest_number.c
+++ Largest_number.c
@@ -5,7 +5,7 @@
 
 int main()
 {
-	int counter=1,number=0,largest=0;
+	int counter=1,number=0,largest=0,second_largest=0;
 
 	while(counter<=10)
 	{
@@ -14,11 +14,12 @@ int main()
 
 		if(number>largest)
 		{
+			second_largest = largest;
 			largest = number;
 		}
 		counter++;
 	}
 
-	printf("The largest number %d\n",largest);
+	printf("The largest number %d\nThe second largest number %d\n",largest,second_largest);
 	return 0;
 }
