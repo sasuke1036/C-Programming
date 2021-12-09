56cf97942c1656a5e2a33b35eb72f113c8f6c6d5
sasuke1036 <yashwanthkota36@gmail.com>
2021-12-09 22:53:31 +0530

    A file which detects the numbers and processes when to stop for the required numbers

---
+++ Validating.c
@@ -0,0 +1,21 @@
+//Libraries
+#include <stdio.h>
+#include <stdbool.h>
+//Main Function
+
+int main()
+{
+	int n;
+
+	while(true)
+	{
+		printf("Enter the value of n : ");
+		scanf("%d",&n);
+
+		if(n==1 || n==2)
+		{
+			break;
+		}
+	}
+return 0;
+}
