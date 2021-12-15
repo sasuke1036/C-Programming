e4af829f4ce67ef61e510433c719080fa4c5e343
sasuke1036 <yashwanthkota36@gmail.com>
2021-12-15 15:37:57 +0530

    Created a file to print out a for loop

---
+++ for.c
@@ -0,0 +1,22 @@
+//Libraries
+#include <stdio.h>
+
+//Main Function
+
+int main()
+{
+	int i,j,k;
+
+	for ( i = 1; i <= 5; i++ ) 
+	{
+		for ( j = 1; j <= 3; j++ )
+		{
+			for ( k = 1; k <= 4; k++ )
+				printf( "*" );
+				printf( "\n" );
+		}
+		printf( "\n" );
+	}
+
+	return 0;
+}
