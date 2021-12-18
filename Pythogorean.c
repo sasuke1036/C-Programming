80d234e0b32252902a473818ba2cb7507028ffc4
sasuke1036 <yashwanthkota36@gmail.com>
2021-12-18 23:05:02 +0530

    Modified the file

--- Pythogorean.c
+++ Pythogorean.c
@@ -7,19 +7,21 @@ int main()
 {
 	int side1,side2,side3,result;
 
+	int n;
+	printf("Enter the number till which do you want to find the pythogorean triplets: ");
+	scanf("%d",&n);
 
-	for(side1=1;side1<500;side1++)
+	for(side1=1;side1<n;side1++)
 	{
-		for(side2=1;side2<500;side2++)
+		for(side2=side1+1;side2<n;side2++)
 		{
-			for(side3=1;side3<500;side3++)
+			for(side3=side2;side3<n;side3++)
 			{
-
 				result = pow(side1,2) + pow(side2,2);
 
 				if(result == pow(side3,2))
 				{
-					printf("%d\t %d\t %d\t",side1,side2,side3);
+					printf("(%d,%d,%d)\n",side1,side2,side3);
 				}
 
 			}
